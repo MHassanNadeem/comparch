f95e17b22505a8ab17c5f72917a088416478eb2b
rizwanvt <36941396+rizwanvt@users.noreply.github.com>
2018-03-07 16:28:55 -0500

    Update varStrideArrayPrefetcher.cpp

--- varStrideArrayPrefetcher.cpp
+++ varStrideArrayPrefetcher.cpp
@@ -11,4 +11,19 @@ VarStrideArrayPrefetcher::~VarStrideArrayPrefetcher(){
 
 void VarStrideArrayPrefetcher::seedMiss(uint64_t pc, uint64_t missAddr){
     /* todo */
+    
+	int curStride = missAddr - lastAddr;
+	int sum = 0;
+
+	for (i=0; i<n-1 ; i++)
+		if (stride[n-1] == stride [i] && curStride == stride[i+1] ) {
+		for (int a = 0; a<prefetchDegree; a++) {
+			sum += stride[i + 2 + a];
+			prefetch(missAddr +sum);
+		}
+	}
+
+	lastAddr = missAddr;
+	stride[n] = curStride;
+	n++;
 }
