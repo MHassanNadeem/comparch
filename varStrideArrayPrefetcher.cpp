5b71244ec06ddd7bf6cf6a5ba7fd145c8e9934cc
MHassanNadeem <me@HassanNadeem.com>
2018-03-18 21:11:17 -0400

    add varStride prefetcher buggy

--- varStrideArrayPrefetcher.cpp
+++ varStrideArrayPrefetcher.cpp
@@ -1,16 +1,74 @@
 #include "varStrideArrayPrefetcher.h"
 
-VarStrideArrayPrefetcher::VarStrideArrayPrefetcher(LRUCache *cache, int prefetchDegree) : Prefetcher(cache, prefetchDegree){
+VarStrideArrayPrefetcher::VarStrideArrayPrefetcher(LRUCache *cache, int prefetchDegree, int num_pc, int num_strides) : Prefetcher(cache, prefetchDegree){
+    this-> num_pc = num_pc;
+    this->num_strides = num_strides;
     
+    /* Initialize the data structure with full capacity,
+       this is to make other functions simpler */
+    for(int i=0; i<num_pc; i++){
+        struct VsEntry vsEntry = {0,0,0,0,0, new uint64_t[num_strides]};
+        q.push_front(vsEntry);
+    }
 }
 
 
 VarStrideArrayPrefetcher::~VarStrideArrayPrefetcher(){
-    
+    for(int i=0; i<num_pc; i++){
+        struct VsEntry vsEntry = q.front();
+        delete[] vsEntry.strideArray;
+    }
 }
 
 void VarStrideArrayPrefetcher::seedMiss(uint64_t pc, uint64_t missAddr){
-    /* todo */
-    
+    struct VsEntry vsEntry;
 
+    /* This iterator will refer to the required struct that contains our pc */
+    list<VsEntry>::iterator itcs = q.end(); /* Initilze to LRU element, which will be updated by default if pc not found */
+
+    /* Find the iterator to the struct in the q that matches our pc  */
+    for (auto it = q.begin(); it != q.end(); it++){
+        if( (*it).pc == pc){
+            itcs = it;
+            break;
+        }
+    }
+    
+    /* PC not found, need to evict LRU struct */
+    if(itcs == q.end()){ /* if pc not found */
+        itcs = q.begin();
+        vsEntry = (*itcs);
+        /* reusing old struct */
+        vsEntry.index = 0;
+        vsEntry.pc = pc;
+    }else{
+        vsEntry = (*itcs);
+    }
+    
+    q.erase(itcs);
+    
+    /* keep track of last two strides */
+    vsEntry.lastStride = vsEntry.currentStride;
+    vsEntry.currentStride = missAddr - vsEntry.missAddr;
+
+    /* struct with pc found */
+    vsEntry.strideArray[(vsEntry.index++)%num_strides] = missAddr;
+    
+    /* try to match */
+    uint64_t start_index = vsEntry.index<num_strides?0:(vsEntry.index%num_strides + 1); // needs to be rechecked
+    uint64_t end_index = vsEntry.index - 1;
+    
+    for(uint64_t i = start_index; i<end_index; i++){
+        uint64_t addr1 = vsEntry.strideArray[i];
+        uint64_t addr2 = vsEntry.strideArray[i+1];
+        
+        if(addr1 == vsEntry.lastStride && addr2 == vsEntry.currentStride){
+            for(int j = 0; j<prefetchDegree; j++){
+                prefetch( vsEntry.strideArray[i+1+1+j] );
+            }
+            break;
+        }
+    }
+
+    q.push_back(vsEntry);
 }
--- varStrideArrayPrefetcher.h
+++ varStrideArrayPrefetcher.h
@@ -7,12 +7,23 @@
 #include "cache.h"
 #include "prefetcher.h"
 
+struct VsEntry{
+    uint64_t pc;
+    uint64_t missAddr;
+    uint64_t lastStride;
+    uint64_t currentStride;
+    // uint64_t lastAddr;
+    uint64_t index;
+    uint64_t* strideArray;
+};
+
 class VarStrideArrayPrefetcher : public Prefetcher{
-    uint64_t lastAddr;
-    int stride;
+    list<VsEntry> q;
+    int num_pc;
+    int num_strides;
     
 public:
-    VarStrideArrayPrefetcher(LRUCache *cache, int prefetchDegree);
+    VarStrideArrayPrefetcher(LRUCache *cache, int prefetchDegree, int num_pc, int num_strides);
     ~VarStrideArrayPrefetcher();
 
     void seedMiss(uint64_t pc, uint64_t addr);
