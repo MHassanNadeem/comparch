5c08f8792b612b641970c68e1d44dc8ce472df3c
MHassanNadeem <me@HassanNadeem.com>
2018-03-19 18:10:02 -0400

    fix bug

--- linkedListPrefetcher.cpp
+++ linkedListPrefetcher.cpp
@@ -12,26 +12,27 @@ LinkedListPrefetcher::~LinkedListPrefetcher(){
 
 
 void LinkedListPrefetcher::seedMiss(uint64_t pc, uint64_t missBlockNumber){
-	/* Youngest address is at the top/begin() of the queue */
+	/* Youngest address is at the bottom/end() of the queue */
 
 	if(map.find(missBlockNumber) != map.end()){
 		/* found */
 		auto it = map[missBlockNumber];
-//		printf("block found\n");
+		cout<<"--block found "<< missBlockNumber<<endl;
+		it++; /* start prefetching from the next block */
 		for(int i = 0; i < prefetchDegree && it != GHBQueue.end(); i++, it++){
 			prefetch(*it);
-//				std::cout <<" next element::"<<*rit << std::endl;
+			cout<<"Prefetching "<<*it<<endl;
 		}
 	}
 
 	if (GHBQueue.size() == sizeGHB){
 		/* remove the oldest address from map and GHB*/
-		map.erase(GHBQueue.back());
-		GHBQueue.pop_back();
+		map.erase(GHBQueue.front());
+		GHBQueue.pop_front();
 	}
 
-	GHBQueue.push_front(missBlockNumber);
-	map[missBlockNumber] = GHBQueue.begin(); /* Keep track of the youngest address in the map */
+	GHBQueue.push_back(missBlockNumber);
+	map[missBlockNumber] = --GHBQueue.end(); /* Keep track of the youngest address in the map */
 }
 
 void LinkedListPrefetcher::printInfo(){
