6008eacafe076abf8d5a76bbc2312bad4762c237
Hassan Nadeem <hnadeem@vt.edu>
2018-05-04 13:30:34 -0400

    give sorted output

--- py-src/run.py
+++ py-src/run.py
@@ -50,29 +50,36 @@ def runBenchmark(fileName, pdRange, prefetchingAlgoRange, isPagePredictionRange)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        fileName = sys.argv[1]
-        fileName = os.path.abspath( fileName )
+        filePath = os.path.abspath( sys.argv[1] )
+        fileName = os.path.split(filePath)[-1]
 
-        if not os.path.isfile(fileName):
-            sys.exit("ERROR: File {0} does not exist".format(fileName))
+        if not os.path.isfile(filePath):
+            sys.exit("ERROR: File {0} does not exist".format(filePath))
 
         if not os.path.isfile(CPP_MODEL_PATH):
             sys.exit("ERROR: CPP Model does not exist")
 
 
         start_time = time.time()
-        results = {
-            'file':os.path.split(fileName)[-1],
-            'results':runBenchmark(fileName, range(0,160,10), [0,1], [0,1]),
-            'time':time.time() - start_time
+        results = runBenchmark(filePath, range(0,160,10), [0,1], [0,1])
+        elapsed_time = time.time() - start_time
+
+        # Arrange results
+        results = sorted(results, key=lambda k: (k['isPage'],k['pa'],k['pd'])) 
+
+        output = {
+            'file':fileName,
+            'filepath':filePath,
+            'results':results,
+            'time':elapsed_time
         }        
 
         # Print result on console
-        print json.dumps(results, indent=4, sort_keys=True)
+        print json.dumps(output, indent=4, sort_keys=True)
 
         # Save results to file
         time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        with open('{0}.json'.format(time), 'w') as outfile:
-            json.dump(results, outfile, indent=4, sort_keys=True)
+        with open('{0}-{1}.json'.format(fileName, time), 'w') as outfile:
+            json.dump(output, outfile, indent=4, sort_keys=True)
     else:
         sys.exit("ERROR: Benchmark file not specified")
